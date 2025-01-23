ad4843e21472f3d4e535a1aa0d3853ab62bb6ad7
Andy Castle <masterchief_and@live.com>
2025-01-23 10:52:39 -0500

    Se hizo una configuración basica del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/homePage/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
 const routes: Routes = [
   {
     path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
   },
 ];
 
